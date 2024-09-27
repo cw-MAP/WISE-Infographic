dd20cd204a10b2a3c35f94015b1dfe447987e97a
Scott Miller <scott.miller@charitywater.org>
2024-09-27 14:26:36 -0400

    Additional updates w/ WISE manual release
    Adjust colors of WISE Scale categories in charts. Breaks itemized responses down by prevalence.

--- WISE_Infographic.R
+++ WISE_Infographic.R
@@ -10,7 +10,7 @@
 ##            Author: Scott Miller              ##
 ##        scott.miller@charitywater.org         ##
 ##                                              ##
-##           Last Updated: 7/16/2024            ##
+##           Last Updated: 9/27/2024            ##
 ##                                              ##
 ##################################################
 
@@ -29,6 +29,8 @@ library(dplyr)
 library(data.table)
 library(ggplot2)
 library(grid)
+library(tidyverse)
+library(htmltools)
 
 
 # ----------------------------------
@@ -36,7 +38,7 @@ library(grid)
 # ----------------------------------
 
 # Set directory: this needs to match the folder where WISE data is stored
-setwd("/Users/scott.miller/Desktop/WISE Scales/WISE Infographics/")
+setwd("/Users/scott.miller/Documents/GitHub/WISE-Infographic/")
 
 # Import Data: this needs to match the data set stored in the directory folder
 data <- read_excel("[Example] Mozambique HWISE Baseline Data.xlsx")
@@ -52,7 +54,7 @@ text_survey <- "Baseline Survey"    # Enter brief survey description
 text_collectedby <- "[Organization]"   # Enter name of organization that collected this data
 text_timing <- "November 2023 (Baseline Survey)"  # Enter the timing of data collection 
 text_geography <- "Project implementation areas within Memba, Larde, Erati, and Moma Districts"     # Describe the geographic coverage of the surveys
-text_samplesize <- "437 Households"
+text_samplesize <- "437 Households. Two-stage simple random sampling of (i) communities and (ii) households."
 
 # ----------------------------------
 # Data Cleaning & Naming
@@ -97,6 +99,10 @@ data <- data %>%
         )
     )
 
+# define color palette for charts
+custom_colors <- c("#ff0000", "#ff7600", "#ffd000", "#0076c3")
+
+
 # ----------------------------------
 # WISE Analysis
 # ----------------------------------
@@ -105,47 +111,68 @@ data <- data %>%
 ## HWISE Classification
 #-----------------------
 
-# create a table with the % of HHs classified as water insecure (HWISE Score < 12)
-prop.table(table(as.character(data$Insecurity_binary))) * 100
-
 # save the percentage of insecure responses
 pct_insecure <- paste(round(prop.table(table(as.character(data$Insecurity_binary)))[2]*100, 1), "%", sep = "")
+pct_nolow <- paste(round(prop.table(table(as.character(data$Insecurity_level)))[1]*100, 1), "%", sep = "")
+pct_mild <- paste(round(prop.table(table(as.character(data$Insecurity_level)))[2]*100, 1), "%", sep = "")
+pct_moderate <- paste(round(prop.table(table(as.character(data$Insecurity_level)))[3]*100, 1), "%", sep = "")
+pct_severe <- paste(round(prop.table(table(as.character(data$Insecurity_level)))[4]*100, 1), "%", sep = "")
 
-# create a table with the % of HHs in each HWISE grouping (Low-to-no, mild, moderate, severe water insecurity)
-prop.table(table(as.character(data$Insecurity_level))) * 100
+# Reshape data to long format, remove NAs, and order the response factor in reverse
+long_data <- data[, grep("_n$", names(data))] %>%
+    pivot_longer(cols = everything(), names_to = "variable", values_to = "response") %>%
+    filter(!is.na(response)) %>%  # Remove NA values
+    mutate(response = factor(response, levels = c("3", "2", "1", "0")))  # Reverse order here
 
+# Calculate percentages for each response category, excluding NAs from total
+percentage_data <- long_data %>%
+    group_by(variable, response) %>%
+    summarise(count = n(), .groups = "drop") %>%
+    group_by(variable) %>%
+    mutate(percentage = count / sum(count) * 100)
 
-# Calculate percentage of observations >= 1 for each variable
-percentages <- colMeans(data[, grep("_n$", names(data))] > 0, na.rm = TRUE) * 100
-
-# Convert to data.table in long format
-long_data <- data.table(variable = names(percentages), percentage = percentages) 
-
-# Create and store bar graph
-Item_Prevalence <- ggplot(long_data, aes(x = variable, y = percentage)) +
-    geom_bar(stat = "identity", fill = "#2074bc") +  # Set all bars to the same custom color
+# Create and store stacked bar graph
+Item_Prevalence <- ggplot(percentage_data, aes(x = variable, y = percentage, fill = response)) +
+    geom_bar(stat = "identity") +
     theme_minimal() +
-    scale_y_continuous(limits = c(0, 100)) +  # Set y-axis limits
+    scale_y_continuous(limits = c(0, 101)) +
     coord_flip() +
     labs(x = "", y = "", title = "") +
     theme(
-        legend.position = "none"  # Remove legend
+        legend.position = "bottom",
+        axis.text.y = element_text(hjust = 1)
+    ) +  
+    guides(color = guide_legend(nrow = 1)) +
+    theme(
+        legend.position = c(0.3, -0.08),  # Adjust these values to move the legend
+        legend.justification = c(0.5, 0.5),
+        legend.direction = "horizontal",
+        legend.box = "horizontal",
     ) +
-    geom_text(aes(label = paste(round(percentage, 1), "%", sep = "")),  # Add this line to annotate bars with their percentage values
-              position = position_dodge(width = 0.9), hjust = 1.3, color = "white") +
-    scale_x_discrete(labels = c("WISE_Worry_n" = "Worry about water", 
-                                "WISE_Angry_n" = "Felt angry about water",
-                                "WISE_Interrupt_n" = "Had supply interuptions",
-                                "WISE_Clothes_n" = "Could not wash clothes",
-                                "WISE_Plans_n" = "Had to change plans",
-                                "WISE_Food_n" = "Had to eat differently",
-                                "WISE_Hands_n" = "Could not wash hands",
-                                "WISE_Body_n" = "Could not wash body",
-                                "WISE_Drink_n" = "Had no water to drink",
-                                "WISE_Sleep_n" = "Went to bed thirsty",
-                                "WISE_None_n" = "Had no water at all",
-                                "WISE_Shame_n" = "Felt shame about water"
-    ))
+    geom_text(aes(label = ifelse(percentage >= 8, paste0(round(percentage, 0), "%"), "")),
+              position = position_stack(vjust = 0.5), color = "white", size = 3) +
+    scale_fill_manual(
+        values = c("3" = "#ff0000", "2" = "#ff7600", "1" = "#ffd000", "0" = "#0076c3"),
+        breaks = c("3", "2", "1", "0"),
+        labels = c("0" = "Never", "1" = "Rarely", "2" = "Sometimes", "3" = "Often/Always"),
+        name = ""
+    ) +
+    scale_x_discrete(labels = c(
+        "WISE_Worry_n" = "Worry about water",
+        "WISE_Angry_n" = "Felt angry about water",
+        "WISE_Interrupt_n" = "Had supply interruptions",
+        "WISE_Clothes_n" = "Could not wash clothes",
+        "WISE_Plans_n" = "Had to change plans",
+        "WISE_Food_n" = "Had to eat differently",
+        "WISE_Hands_n" = "Could not wash hands",
+        "WISE_Body_n" = "Could not wash body",
+        "WISE_Drink_n" = "Had no water to drink",
+        "WISE_Sleep_n" = "Went to bed thirsty",
+        "WISE_None_n" = "Had no water at all",
+        "WISE_Shame_n" = "Felt shame about water"
+    )) +
+    guides(fill = guide_legend(reverse = TRUE))
+
 
 
 
@@ -171,16 +198,21 @@ hhsize_data <- data %>%
 Insecurity_by_HHsize <- ggplot(hhsize_data, aes(x = HHsize_group, y = percentage, fill = Insecurity_level)) +
     geom_bar(stat = "identity") +
     labs(x = "Household Size", y = "Percentage") +
-    geom_text(data = . %>% filter(percentage >= 5),  # Only label percentages >= 5%
+    geom_text(data = . %>% filter(percentage >= 8),  # Only label percentages >= 8% (otherwise bars are too small for text)
               aes(label = paste(round(percentage, 0), "%", sep = "")),
               position = position_stack(vjust = 0.5), color = "white", size = 3) +
     theme_minimal() +
-    scale_fill_brewer(palette = "Blues") +  # You can change the color palette as needed
+    scale_fill_manual(values = custom_colors,
+                      labels = c("Severe", 
+                                 "Moderate", 
+                                 "Mild", 
+                                 "No-to-low")) +  # You can change the color palette as needed
     scale_y_continuous(labels = scales::percent_format(scale = 1)) +  # Format y-axis as percentages
     theme(
         axis.text.x = element_text(hjust = 1),  # Rotate x-axis labels for better readability
         legend.position = "none",  # Move legend to bottom
-    )
+    ) + 
+    guides(fill = guide_legend(reverse = TRUE))
 
 
 #-----------------------
@@ -192,13 +224,6 @@ district_data <- data %>%
     group_by(District) %>%
     summarize(pct_insecure = mean(Insecurity_binary == "Water Insecure", na.rm = TRUE) * 100)
 
-# Create the ggplot bar graph
-Insecurity_by_District <- ggplot(district_data, aes(x = District, y = pct_insecure)) +
-    geom_bar(stat = "identity", fill = "#2074bc") +
-    labs(x = "District", y = "Insecurity") +
-    geom_text(aes(label = paste(round(pct_insecure, 1), "%", sep = "")),  # Add this line to annotate bars with their percentage values
-              position = position_dodge(width = 0.9), vjust = 1.3, color = "white") +
-    theme_minimal()
 
 # Calculate the percentage for each Insecurity_level within each HHsize_group, excluding NAs
 district_data <- data %>%
@@ -212,16 +237,21 @@ district_data <- data %>%
 Insecurity_by_District <- ggplot(district_data, aes(x = District, y = percentage, fill = Insecurity_level)) +
     geom_bar(stat = "identity") +
     labs(x = "District", y = "Percentage", fill = "") +
-    geom_text(data = . %>% filter(percentage >= 5),  # Only label percentages >= 5%
+    geom_text(data = . %>% filter(percentage >= 8),  # Only label percentages >= 8% (otherwise bars are too small for text)
               aes(label = paste(round(percentage, 0), "%", sep = "")),
               position = position_stack(vjust = 0.5), color = "white", size = 3) +
     theme_minimal() +
-    scale_fill_brewer(palette = "Blues") +  # You can change the color palette as needed
+    scale_fill_manual(values = custom_colors,
+                      labels = c("Severe", 
+                                 "Moderate", 
+                                 "Mild", 
+                                 "No-to-low")) +  # You can change the color palette as needed
     scale_y_continuous(labels = scales::percent_format(scale = 1)) +  # Format y-axis as percentages
     theme(
         axis.text.x = element_text(hjust = 1),  # Rotate x-axis labels for better readability
         legend.position = "bottom",  # Move legend to bottom
-    )
+    ) +
+    guides(fill = guide_legend(reverse = TRUE))
 
 
 # ------------------------------------------------------------------------------
@@ -237,14 +267,14 @@ pdf("Infographic.pdf", width = 10, height = 14)
 
 grid.newpage() 
 pushViewport(viewport(layout = grid.layout(9, 4)))
-grid.rect(gp = gpar(fill = "#E2E2E3", col = "#E2E2E3"))
+grid.rect(gp = gpar(fill = "white", col = "white"))
 
 
 #-----------------------
-# Header - grey background with Title
+# Header - white background with Title
 #-----------------------
 
-grid.rect(gp = gpar(fill = "grey", col = "grey", alpha = 0.6), 
+grid.rect(gp = gpar(fill = "white", col = "white", alpha = 0.6), 
           x = unit(0.5, "npc"), 
           y = unit(0.95, "npc"), 
           width = unit(1, "npc"), 
@@ -254,18 +284,18 @@ grid.text("Water Insecurity Experiences (WISE) Scales",
           y = unit(0.98, "npc"), 
           x = unit(0.5, "npc"), 
           vjust = 3, hjust = .5, 
-          gp = gpar(col = "#2074bc", cex = 2.4, alpha = 1))
+          gp = gpar(col = "#0076c3", cex = 2.4, alpha = 1))
 
 grid.text(paste(text_country, text_survey, sep = " "), 
           y = unit(0.88, "npc"), 
-          gp = gpar(col = "#2074bc", cex = 2.2))
+          gp = gpar(col = "#0076c3", cex = 2.2))
 
 
 #-----------------------
 # Subheader - Blue background with data details
 #-----------------------
 
-grid.rect(gp = gpar(fill = "#2074bc", col = "#2074bc", alpha = 0.6), 
+grid.rect(gp = gpar(fill = "#0076c3", col = "#0076c3", alpha = 0.57), 
           x = unit(0.5, "npc"), 
           y = unit(0.78, "npc"), 
           width = unit(1, "npc"), 
@@ -280,10 +310,10 @@ grid.text("",
 grid.text(paste(
     "",
     "Scale:",
-    "Collected by:",
+    "Collected By:",
     "Collection Timing:",
     "Geographic Coverage:",
-    "Sample Size:",
+    "Sample Size & Details:",
     "",
     "", sep = "\n"), vjust = 0, hjust = 0, 
     x = unit(0.05, "npc"), 
@@ -338,24 +368,27 @@ grid.text(paste(
     paste("Who is water insecure in ",text_country, "?", sep = ""),
     "", sep = "\n"), vjust = 0, hjust = 0, 
     x = unit(0.05, "npc"), 
-    y = unit(0.53, "npc"), 
+    y = unit(0.535, "npc"), 
     gp = gpar(col = "Black", cex = 1.3, fontface = "bold"))
 
 
 grid.text(paste(
     pct_insecure,
     "", sep = "\n"), vjust = 0, hjust = 0, 
-    x = unit(0.05, "npc"), 
-    y = unit(0.5, "npc"), 
-    gp = gpar(col = "#2074bc", cex = 1.7, fontface = "bold"))
+    x = unit(0.052, "npc"), 
+    y = unit(0.5075, "npc"), 
+    gp = gpar(col = "#0076c3", cex = 1.5, fontface = "bold"))
 
 grid.text(paste(
-    "                   of households participating in the survey",
-    "experienced moderate-to-high water insecurity in 2023,",
-    "with experiences varying by household size and district.",
-    "", sep = "\n"), vjust = 0, hjust = 0, 
+    "                   of households participating in the survey exper-", "\n",
+    "ienced moderate-to-high water insecurity in 2023. ", pct_severe, "\n",
+    "of respondents' water insecurity experiences were classified", "\n",
+    "as 'severe,' while ", pct_moderate, " of respondents were considered", "\n",
+    "'moderately' insecure. ", pct_mild, " were considered 'mildly' insecure,", "\n",
+    "while ", pct_nolow, " experienced no-to-low water insecurity.",
+    sep = ""), vjust = 0, hjust = 0, 
     x = unit(0.05, "npc"), 
-    y = unit(0.48, "npc"), 
+    y = unit(0.45, "npc"), 
     gp = gpar(col = "Black", cex = 1))
 
 #-----------------------
@@ -441,9 +474,8 @@ grid.text(paste(
     "monitor progress and accountability.",
     sep = "\n"), vjust = 0, hjust = 0, 
     x = unit(0.53, "npc"), 
-    y = unit(0.025, "npc"), 
+    y = unit(0.02, "npc"), 
     gp = gpar(col = "Black", cex = 1))
 
 dev.off()
-
 
